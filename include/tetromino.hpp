a81ff4b46e71f07d05e6d84c471b86591d55f8ab
Guilherme Gelmi <ggelmisalvo@gmail.com>
2024-12-25 23:33:33 +0100

    refactor: mockserver now uses dynamically allocated objects

--- include/tetromino.hpp
+++ include/tetromino.hpp
@@ -1,25 +1,19 @@
 #ifndef TETROMINO_HPP
 #define TETROMINO_HPP
 
+#include <array>
 #include "entity.hpp"
 
 class Tetromino : public Entity {
     public:
-        Tetromino(SDL_Color color, unsigned int shape[16])
+        Tetromino(SDL_Color color, std::array<unsigned int, 16> shape)
             : Entity(4, 4) {
             this->color.r = color.r;
             this->color.g = color.g;
             this->color.b = color.b;
             this->color.a = color.a;
             for(int i = 0; i < 16; i++) this->tiles[i] = shape[i];
-        };
-        Tetromino() : Entity(4,4) {
-            this->color.r = 255;
-            this->color.g = 255;
-            this->color.b = 255;
-            this->color.a = 255;
-            for(int i = 0; i < 16; i++) this->tiles[i] = 1;    
-        }
+        }; 
         void moveDir(int dir);
         void moveTo(int x, int y);
         SDL_Color getColor();
@@ -27,11 +21,12 @@ class Tetromino : public Entity {
 
 class TetrominoI : public Tetromino {
     public:
-        TetrominoI() {
-            unsigned int shape[16] = {0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0};
-            SDL_Color color = {52, 235, 235, 255};
-            Tetromino(color, shape);
-        };
+        TetrominoI() : Tetromino({52,235,235,255}, {0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0}) {}; 
+};
+
+class TetrominoL : public Tetromino {
+    public:
+        TetrominoL() : Tetromino({52,235,235,255}, {0,0,0,0,1,1,1,1,0,0,0,1,0,0,0,1}) {}; 
 };
 
 #endif
--- src/mockserver.cpp
+++ src/mockserver.cpp
@@ -1,10 +1,10 @@
 #include "mockserver.hpp"
 
 MockServer::MockServer() {
-    this->board = Board();
-    this->tetromino = TetrominoI();
-    printf("%d\n", this->tetromino.getColor().r);
-    for(int i = 0; i< 16; i++) printf("%d ", this->tetromino.getTiles()[i]);
+    this->board = new Board();
+    this->tetromino = new TetrominoI();
+    printf("%d\n", this->tetromino->getColor().r);
+    for(int i = 0; i< 16; i++) printf("%d ", this->tetromino->getTiles()[i]);
     printf("\n");
     this->counter = 0;
 }
@@ -13,29 +13,29 @@ void MockServer::update(int command) {
     this->counter = this->counter + 1; 
     if(command == 0) {
     } else if(command == 2) {
-        while(board.isPositionValid(tetromino) == 0){
-            tetromino.moveDir(0);
+        while(board->isPositionValid(*tetromino) == 0){
+            tetromino->moveDir(0);
         }
-        tetromino.moveDir(3);
-        board.lockTetromino(tetromino);
-        tetromino.moveTo(0,0);
+        tetromino->moveDir(3);
+        board->lockTetromino(*tetromino);
+        tetromino->moveTo(0,0);
     } else {
-        tetromino.moveDir(command);
-        int position = board.isPositionValid(tetromino);
-        if(position != 0) tetromino.moveDir(-command);
+        tetromino->moveDir(command);
+        int position = board->isPositionValid(*tetromino);
+        if(position != 0) tetromino->moveDir(-command);
         if(position == 2) {
-            board.lockTetromino(tetromino);
-            tetromino.moveTo(0,0);
+            board->lockTetromino(*tetromino);
+            tetromino->moveTo(0,0);
         }
     }
     if(counter >= 1000){
         counter = 0;
-        tetromino.moveDir(0);
-        int position = board.isPositionValid(tetromino);
+        tetromino->moveDir(0);
+        int position = board->isPositionValid(*tetromino);
         if(position != 0) {
-            tetromino.moveDir(3);
-            board.lockTetromino(tetromino);
-            tetromino.moveTo(0, 0);
+            tetromino->moveDir(3);
+            board->lockTetromino(*tetromino);
+            tetromino->moveTo(0, 0);
         }
     }
 }
@@ -43,14 +43,14 @@ void MockServer::update(int command) {
 GameData MockServer::getState(){
     GameData gameData;
     for(int i = 0; i < 200; i++) {
-        gameData.board[i] = this->board.getTiles()[i];
+        gameData.board[i] = this->board->getTiles()[i];
         if(i<16) {
-            gameData.tetromino[i] = this->tetromino.getTiles()[i];
+            gameData.tetromino[i] = this->tetromino->getTiles()[i];
         }
     }
-    //printf("%d\n", this->tetromino.getColor().r);
-    gameData.color = this->tetromino.getColor();
-    gameData.x = tetromino.getRect()->x;
-    gameData.y = tetromino.getRect()->y;
+    //printf("%d\n", this->tetromino->getColor()->r);
+    gameData.color = this->tetromino->getColor();
+    gameData.x = tetromino->getRect()->x;
+    gameData.y = tetromino->getRect()->y;
     return gameData;
 }
